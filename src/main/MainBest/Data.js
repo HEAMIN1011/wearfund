3c89484962f1581cdd1560e7f0d9f9b6f1d61fa2
janghyun1019 <mon.p.a.hyun@gmail.com>
2024-12-10 14:32:37 +0900

    메인 bestfund 데이터 작업

---
+++ src/main/MainBest/Data.js
@@ -0,0 +1,70 @@
+// Data.js
+const FundingData = [
+  {
+    id: 1,
+    title: "[레인코트] 일상과 아웃도어를 이어주는 방수+방풍 아우터 2종",
+    price: "219,000원",
+    percentage: "1,881%",
+    daysLeft: "11일 남음",
+    imageUrl: `./image/path-to-image-1.jpg`,
+  },
+  {
+    id: 2,
+    title: "[레인코트] 더 업그레이드 된 기능성! 방수+방풍+투습 레인-드라이코트",
+    price: "189,000원",
+    percentage: "2,369%",
+    daysLeft: "12일 남음",
+    imageUrl: "./image/path-to-image-2.jpg",
+  },
+  {
+    id: 3,
+    title: "[점퍼] 솜보다 가벼운, 밸롭 에센셜 히트 에어볼 숏패딩",
+    price: "49,000원",
+    percentage: "3,963%",
+    daysLeft: "18일 남음",
+    imageUrl: "./image/path-to-image-3.jpg",
+  },
+  {
+    id: 4,
+    title: "[점퍼] 베이직한 디자인, 활용도 높은 밸롭 슈드 다운 경량패딩",
+    price: "129,000원",
+    percentage: "118%",
+    daysLeft: "12일 남음",
+    imageUrl: "./image/path-to-image-4.jpg",
+  },
+  {
+    id: 5,
+    title: "[셔츠] 세미오버핏 | 여름에도 답답하지 않은 레이온 실크 셔츠",
+    price: "54,900원",
+    percentage: "4,241%",
+    daysLeft: "9일 남음",
+    imageUrl: "./image/path-to-image-5.jpg",
+  },
+  {
+    id: 6,
+    title: "[정장] 한벌로 사계절&만능 코디 거뜬! 포시즌 국민 수트 6컬러",
+    price: "39,200원",
+    percentage: "3,025%",
+    daysLeft: "15일 남음",
+    imageUrl: "./image/path-to-image-6.jpg",
+  },
+  {
+    id: 7,
+    title: "[코트] 한정수량앵콜 달의 추위를 견딘 충전재x방수+방풍+투습 페트드라이코트",
+    price: "358,000원",
+    percentage: "702%",
+    daysLeft: "6일 남음",
+    imageUrl: "./image/path-to-image-7.jpg",
+  },
+  {
+    id: 8,
+    title: "[생활한복]가장 현대적인한복 창의 로브가디건 바람이 휘날릴때 특별해집니다",
+    price: "96,000원",
+    percentage: "10,216%",
+    daysLeft: "10일 남음",
+    imageUrl: "./image/path-to-image-8.jpg",
+  },
+   
+];
+
+export default FundingData;
