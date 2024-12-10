0226d8d153b132f1f44ab5ec5bcdea142fab4138
Han-kyu <kwakbab9@gmail.com>
2024-12-10 14:25:45 +0900

    Information 클릭 시 이동 할 페이지 작업 및 Navigation Bar 적용 테스트

---
+++ src/CommPages/CommInfo.js
@@ -0,0 +1,58 @@
+import './CommInfo.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX, faPen } from "@fortawesome/free-solid-svg-icons";
+
+import { useParams } from "react-router-dom";
+function CommInfo({info}) {
+    
+    let {id} = useParams();
+
+    let infopara = info.find((item) => {
+        return item.id == id;
+    });
+
+
+    return (
+        <div>
+            <div className="infotop">
+                <span className='infotoptext'>
+                    <p>패션에</p>
+                    <p>투자하다 !</p>
+                </span>
+                <div className='infotopimg'>
+
+                </div>
+            </div>
+
+            <br /><br />
+
+            <div className='bodywrap'>
+                <div className='infobody'>
+                    <h4>공지사항</h4><br />
+
+                    <h5>{infopara.title}</h5><br/>
+
+                    <h6>공지사항</h6>
+                    <br />
+                    <img src={infopara.imgPath}></img>
+                    <p>{infopara.content}</p>
+                </div>
+                
+
+                <hr style={{ width: '60%', margin: 'auto', marginTop: '10px', marginBottom: '5px' }}></hr>
+                        <div className="modalfooter">
+                            <div className="commentcount"><span>댓글 0</span></div>
+                            <div className="commentwrap">
+                                <textarea readOnly placeholder="로그인이 필요합니다" className="commentbox"></textarea>
+                            </div>
+                            <div className="commentwritewrap"><button className="commentwrite"
+                            ><FontAwesomeIcon icon={faPen} /> 댓글 달기</button></div>
+                            </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default CommInfo;
